b21714ac2bed8885d3513128f8010e54cdf06795
itzyashh <yashjadhav1411@rocketmail.com>
2022-09-26 23:11:49 +0530

    Publish to expo

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,29 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { MaterialIcons } from "react-native-vector-icons";
 
 const Card = ({ title, id, navigation }) => {
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("Show", { id })}>
+    <TouchableOpacity
+      style={{
+        marginTop: 20,
+        marginBottom: 10,
+        marginHorizontal: 20,
+      }}
+      onPress={() => navigation.navigate("Show", { id })}
+    >
       <View style={styles.container}>
         <Text style={styles.text}>
           {title}-{id}
         </Text>
+        <TouchableOpacity
+          style={styles.deleteButton}
+          onPress={() => console.log("Deleted" + id)}
+        >
+          <View>
+            <MaterialIcons style={styles.icon} name="delete" size={25} />
+          </View>
+        </TouchableOpacity>
       </View>
     </TouchableOpacity>
   );
@@ -17,16 +33,26 @@ export default Card;
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
-    marginBottom: 10,
-    marginHorizontal: 20,
-    padding: 24,
     borderRadius: 6,
     backgroundColor: "#DCD6F7",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   text: {
+    padding: 24,
     fontSize: 16,
     color: "#424874",
     fontWeight: "500",
+  },
+  deleteButton: {
+    backgroundColor: "#424874",
+    width: 40,
+    borderTopRightRadius: 6,
+    borderBottomRightRadius: 6,
+    justifyContent: "center",
+  },
+  icon: {
+    alignSelf: "center",
+    color: "#F4EEFF",
   },
 });
